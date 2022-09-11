299e9379d07fe23352c89679222f7e1af847da76
Gianfranco Palumbo <gianpa@gmail.com>
2022-09-11 15:34:43 +0100

    LanguageSwitcher

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,26 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: [
+    //
+    "@nuxt/content",
+    "@intlify/nuxt3",
+  ],
   // disable generating the shim for *.vue files
   typescript: {
     shim: false,
   },
+  intlify: {
+    localeDir: 'locales', // set the `locales` directory at source directory of your Nuxt application
+    // vueI18n: {
+    //   // ...
+    // }
+  }
+  // chakra: {
+  //   extendTheme: {
+  //     colors: {
+  //       brand: { /* ... */ }
+  //     }
+  //   }
+  // }
 });
