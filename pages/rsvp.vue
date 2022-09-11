5cbd38f4d991c79f3c76b5f239b2b2807a968e6c
Gianfranco Palumbo <gianpa@gmail.com>
2022-09-11 15:36:50 +0100

    /rsvp page

---
+++ pages/rsvp.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>RVSP</h1>
+    <p>{{ t('hi') }}</p>
+    <LanguageSwitcher />
+</template>
+
+
+<script setup lang="ts">
+    import { useI18n } from 'vue-i18n';
+    const { t } = useI18n();
+</script>
+<script lang="ts">
+    import LanguageSwitcher from '../components/LanguageSwitcher'
+    export default {
+        name: 'RSVP',
+        components: {
+            LanguageSwitcher
+        }
+}
+</script>
