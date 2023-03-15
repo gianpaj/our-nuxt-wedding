77cfe245190848c1f5568041c11f59119faff669
Gianfranco Palumbo <gianpa@gmail.com>
2023-03-15 10:28:43 +0100

    fix build

--- components/Input.client.vue
+++ components/Input.client.vue
@@ -1,27 +1,26 @@
 <template>
-  <input v-model="question" <!-- v-on:wheel="handleWheel" -->
-  v-type="type">
+  <input v-model="question" v-on:wheel="handleWheel" v-type="type" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      type: 'text',
-      question: '',
-    }
+      type: "text",
+      question: "",
+    };
   },
   watch: {
     // whenever question changes, this function will run
     question(question) {
-      console.log('question', question);
-    }
+      console.log("question", question);
+    },
   },
   methods: {
     handleWheel() {
       // console.log('question', this.question);
       // this.question=1
-    }
+    },
   },
   created() {
     // window.addEventListener('wheel', this.handleWheel);
@@ -29,5 +28,5 @@ export default {
   destroyed() {
     // window.removeEventListener('wheel', this.handleWheel);
   },
-}
+};
 </script>
